d5aee84dbb61dbe355e81af88098b1a6ca021b71
ilaure <ilaure89@gmail.com>
2019-05-30 23:48:48 +0300

    Add files via upload

---
+++ 008-ComprehensionComprehensionCheckSmoothing.R
@@ -0,0 +1,56 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(purrr)
+install.packages("pdftools")
+library(pdftools)
+library(lubridate)
+library(magrittr)
+library(dplyr)
+
+fn <- system.file("extdata", "RD-Mortality-Report_2015-18-180531.pdf", package="dslabs")
+dat <- map_df(str_split(pdf_text(fn), "\n"), function(s){
+  s <- str_trim(s)
+  header_index <- str_which(s, "2015")[1]
+  tmp <- str_split(s[header_index], "\\s+", simplify = TRUE)
+  month <- tmp[1]
+  header <- tmp[-1]
+  tail_index  <- str_which(s, "Total")
+  n <- str_count(s, "\\d+")
+  out <- c(1:header_index, which(n==1), which(n>=28), tail_index:length(s))
+  s[-out] %>%
+    str_remove_all("[^\\d\\s]") %>%
+    str_trim() %>%
+    str_split_fixed("\\s+", n = 6) %>%
+    .[,1:5] %>%
+    as_data_frame() %>% 
+    setNames(c("day", header)) %>%
+    mutate(month = month,
+           day = as.numeric(day)) %>%
+    gather(year, deaths, -c(day, month)) %>%
+    mutate(deaths = as.numeric(deaths))
+}) %>%
+  mutate(month = recode(month, "JAN" = 1, "FEB" = 2, "MAR" = 3, "APR" = 4, "MAY" = 5, "JUN" = 6, 
+                        "JUL" = 7, "AGO" = 8, "SEP" = 9, "OCT" = 10, "NOV" = 11, "DEC" = 12)) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  filter(date <= "2018-05-01",deaths !="na" )
+
+total_days <- diff(range(as.numeric(dat$date)))
+span <- 60/total_days
+fit <- loess(deaths~as.numeric(dat$date), degree = 1, span = span, data = dat)
+dat %>% mutate(smooth = fit$fitted) %>%
+  ggplot(aes(as.numeric(dat$date), deaths)) +
+  geom_point(size = 3, alpha = .5, color = "grey") +
+  geom_line(aes(as.numeric(dat$date),smooth, color="red"))
+
+#q2
+dat %>% 
+  mutate(smooth = predict(fit, as.numeric(date)), day = yday(date), year = as.character(year(date))) %>%
+  ggplot(aes(day, smooth, col = year)) +
+  geom_line(lwd = 2)
+
+#q3
+mnist_27$train %>% 
+  mutate(y = ifelse(y=="7", 1, 0)) %>%
+  ggplot(aes(x_2, y)) + 
+  geom_smooth(method = "loess")
+#ANSWER: There is predictive power and the conditional probability is non-linear.
---
+++ 009-ComprehensionCheckWorkingWithMatrices.R
@@ -0,0 +1,84 @@
+##Comprehension Check: Working with Matrices##
+#Q1
+#1 point possible (graded)
+#Which line of code correctly creates a 100 by 10 matrix of randomly generated normal numbers and assigns it to x?
+
+x <- matrix(rnorm(100*10), 100, 10)
+
+# Q2
+# Write the line of code that would give you the specified information about the matrix x 
+# that you generated in q1. Do not include any spaces in your line of code.
+
+# Dimension of x
+dim(x)
+
+# Number of rows of x.
+length(x[,1])
+nrow(x)  # OK
+
+# Number of columns of x.
+length(x[1,])
+ncol(x)  # OK
+
+
+#Q3
+#Which of the following lines of code would add the scalar 1 to row 1, the scalar 2 to row 2, and so on, for the matrix x?
+x1 <- x
+x1 <- x1 + seq(nrow(x))  # OK
+
+x2 <- x
+x2 <- sweep(x2, 1, 1:nrow(x),"+")   # OK
+
+
+#Q4
+#Which of the following lines of code would add the scalar 1 to column 1, the scalar 2 to column 2, and so on, for the matrix x?
+
+x <- sweep(x, 2, 1:ncol(x), FUN = "+")  # OK
+
+#Q5
+#Which code correctly computes the average of each row of x?
+
+rowMeans(x)  # OK
+
+# Which code correctly computes the average of each column of x?
+colMeans(x)
+
+#Q6
+#For each digit in the mnist training data, compute the proportion of pixels that are in the grey area, defined as values between 50 and 205. (To visualize this, you can make a boxplot by digit class.)
+#What proportion of pixels are in the grey area overall, defined as values between 50 and 205?
+
+library(dslabs)
+mnist <- read_mnist()
+head(mnist)
+
+x <- mnist$train$images[1:1000,] 
+y <- mnist$train$labels[1:1000]
+
+# TEST
+grid <- matrix(x[3,], 28, 28)
+image(1:28, 1:28, grid)
+image(1:28, 1:28, grid[,28:1])
+
+library(tidyverse)
+
+avg <- rowMeans(x)
+data.frame(labels = as.factor(y), row_averages = avg) %>%
+  ggplot(aes(labels, row_averages)) +
+  geom_boxplot()
+
+
+# binarizing the data
+bin_x <- x
+bin_x[bin_x < 50  |  bin_x >  205] <- 0 
+bin_x[bin_x >= 50 &  bin_x <= 205] <- 1
+
+avg_gray <- rowMeans(bin_x)
+data.frame(labels = as.factor(y), row_averages = avg_gray) %>%
+  ggplot(aes(labels, row_averages)) +
+  geom_boxplot()
+
+mean(bin_x)  # OK
+
+# Other way
+avg_gray2 <- rowMeans(x >= 50 & x <= 205)
+mean(avg_gray2)
